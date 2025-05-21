ee9022c251129e798b1ead65b8c29717f97fbc0d
Nirav Parmar <54586517+Nirav0702@users.noreply.github.com>
2025-05-21 16:15:49 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,7 @@
+def main():
+  print(”Hello, Github Actions !!!”)
+  print(”This is a simple Python script”)
+  print(”It runs as part of an automated workflow”)
+
+if __name__ == “__main__”:
+  main()
